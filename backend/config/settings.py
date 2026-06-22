06b6c58924017b8a5a87d76cdf077065589a232c
salam lakhan <119106199+salamlakhan7@users.noreply.github.com>
2026-06-22 12:18:18 +0500

    Update PostgreSQL database settings for flexibility

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -117,12 +117,18 @@ DATABASE_ENGINE = os.environ.get("DB_ENGINE", "sqlite").lower()
 if DATABASE_ENGINE == "postgresql":
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("POSTGRES_DB", "ab_medical_assistance"),
-            "USER": os.environ.get("POSTGRES_USER", "postgres"),
-            "PASSWORD": os.environ.get("POSTGRES_PASSWORD", ""),
-            "HOST": os.environ.get("POSTGRES_HOST", "127.0.0.1"),
-            "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+           # "ENGINE": "django.db.backends.postgresql",
+           # "NAME": os.environ.get("POSTGRES_DB", "ab_medical_assistance"),
+           # "USER": os.environ.get("POSTGRES_USER", "postgres"),
+           # "PASSWORD": os.environ.get("POSTGRES_PASSWORD", ""),
+           # "HOST": os.environ.get("POSTGRES_HOST", "127.0.0.1"),
+           # "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+
+            "NAME": os.environ.get("POSTGRES_DB") or os.environ.get("PGDATABASE"),
+            "USER": os.environ.get("POSTGRES_USER") or os.environ.get("PGUSER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD") or os.environ.get("PGPASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST") or os.environ.get("PGHOST"),
+            "PORT": os.environ.get("POSTGRES_PORT") or os.environ.get("PGPORT", "5432"),
         }
     }
 else:
