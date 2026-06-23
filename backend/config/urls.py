655a389982f65ae7ff65b9c9bb4aa380f25b2f2c
salam lakhan <119106199+salamlakhan7@users.noreply.github.com>
2026-06-23 12:49:11 +0500

    Comment out static file serving in DEBUG mode
    Comment out the conditional static file serving in urls.py.

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path("api/", include("apps.products.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
